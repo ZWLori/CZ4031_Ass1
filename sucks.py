8c4cfd84925309ed52dd3a0e53215c9c5991e70f
ZWLori <ZWLori@ZWLoris-MacBook-Pro.local>
2017-09-22 21:09:49 +0800

    Modification

--- sucks.py
+++ sucks.py
@@ -3,52 +3,40 @@ import csv
 
 class OurContentHandler(xml.sax.ContentHandler):
 
-    # month??????
-
     def __init__(self, csv_writer):
         xml.sax.ContentHandler.__init__(self)
-        self.pub_table = {}
-        self.authors = []
-        self.field = {}
-        self.tag = ""
-        self.key = ""
         self.writer = csv_writer
 
     def startElement(self, name, attrs):
         self.tag = name
-        subclasses = ["incollection", "proceedings", "inproceedings", "article", "book"]
-        if name in subclasses :
+        self.subclasses = ["incollection", "proceedings", "inproceedings", "article", "book", "www", "phdthesis", "mastersthesis"]
+        if name in self.subclasses :
+            self.field = {"key": "", "title": "", "year": "", "authors": []}
             if attrs:
-                self.key = attrs.getValue("key")
-                self.pub_table["type"] = name
-                self.pub_table["key"] = self.key
-                self.field["key"] = self.key
+                self.field["key"] = attrs.getValue("key")
 
     def characters(self, content):
         if content.strip():
             attr_list = ["year", "title"]
             if self.tag == "author":
-                self.authors.append(content)
+                self.field["authors"].append(content)
             if self.tag in attr_list:
                 self.field[self.tag] = content
 
     def endElement(self, name):
         # write to csv file
-        subclasses = ["incollection", "proceedings", "inproceedings", "article", "book"]
-        if name in subclasses :
-            self.field["author"] = self.authors
-            row = [self.field["key"], self.field["title"],
-                   self.field["year"], ','.join(self.field["author"])]
-            print(row)
+        if name in self.subclasses :
+            row = [name, self.field["key"], self.field["title"],
+                   self.field["year"], ','.join(self.field["authors"])]
             self.writer.writerow(row)
 
 
 def main(sourceFileName):
     csv_file = open('./field.csv', 'w')
     csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(["key", "title", "year", "author"])
+    csv_writer.writerow(["class", "key", "title", "year", "authors"])
     source = open(sourceFileName)
     xml.sax.parse(source, OurContentHandler(csv_writer))
 
 if __name__ == "__main__":
-    main("./dblp.xml")
+    main("/Users/ZWLori/Downloads/dblp.xml")
